a99058243c62591aefab7c0c407141b0c09019e0
Zach Johnston <5273704+zachjohnston@users.noreply.github.com>
2019-04-23 19:00:44 -0400

    feat: Fade in an image over 1000 ms

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Animated } from 'react-native';
+
+// decay - initial velocity and slow to a stop
+// spring - spring physics model
+// timing - animated a value over time
 
 export default class App extends React.Component {
+  state ={
+    opacity: new Animated.Value(0)
+  }
+
+  componentDidMount() {
+    const { opacity } = this.state
+
+    Animated.timing(opacity, { toValue: 1, duration: 1000 })
+      .start()
+  }
+
   render() {
+    const { opacity } = this.state
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Animated.Image 
+          style={[styles.img, { opacity }]}
+          source={{uri: 'https://tylermcginnis.com/tylermcginnis_glasses-300.png'}}
+        />
       </View>
     );
   }
@@ -18,4 +38,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img: {
+    width: 200,
+    height: 200
+  }
 });
